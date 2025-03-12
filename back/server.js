862b4e4792e9009acf96c97a797a620d7f9c1d0a
CptWickie <OcegueraFiliberto@gmail.com>
2025-03-11 20:01:20 -0700

    Updated backend setup and structure

--- back/server.js
+++ back/server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Mock API Endpoints
 app.get('/api/fire-risk', async (req, res) => {
     res.json({ message: "Fire risk data (mock)" });
 });
